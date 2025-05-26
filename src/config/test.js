eaaae228f5c9936c74fce7a8c2a865d2c8eba2b3
transcodedilip88 <transcode.dilip@gmail.com>
2025-05-26 14:59:29 +0530

    Update test.js

--- src/config/test.js
+++ src/config/test.js
@@ -1,8 +1,1 @@
-module.exports ={
-    port: 3300,
-    dbUrl: 'mongodb://127.0.0.1:27017/food-delevery',
-    jwtSecrets: {
-      verify:'6Uu&PoR7wBQRJesHFK43^y5WBbAM72!myFDLUA^Ti7Eg4%oRCTxj&JnM7@iqyLDg%@Y@uL@Y$DLsWpcC',
-      main:'G&&H^qfC9HcExeDfDex5JLJLTfPdFxcim%*f^nop2LFbHVU%h!Mb6THEHr&htCiS%3#*W2L@2%TKVnYq'
-    },
-}
+
