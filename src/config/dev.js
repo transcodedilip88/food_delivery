b569f4aa536eba6fc0e014418b7ceca5387b855e
transcodedilip88 <transcode.dilip@gmail.com>
2025-05-26 14:59:42 +0530

    Update dev.js

--- src/config/dev.js
+++ src/config/dev.js
@@ -1,12 +1,1 @@
-module.exports ={
-    port: 4500,
-    dbUrl: 'mongodb://127.0.0.1:27017/food-delevery',
-    jwtSecrets: {
-      verify:'6Uu&PoR7wBQRJesHFK43^y5WBbAM72!myFDLUA^Ti7Eg4%oRCTxj&JnM7@iqyLDg%@Y@uL@Y$DLsWpcC',
-      main:'G&&H^qfC9HcExeDfDex5JLJLTfPdFxcim%*f^nop2LFbHVU%h!Mb6THEHr&htCiS%3#*W2L@2%TKVnYq'
-    },
-    clientID :"633345148840-74p4ohgal63ka1dmhvr3hpdulror4hjc.apps.googleusercontent.com",
-    clientSecret:"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",
-    refreshToken:"1//04ON7Z0cIQjy6CgYIARAAGAQSNwF-L9IrUhDv9xLyUn-qMkIeAELqQ5N9ZYkw6e-fUK2Dvi_iNtHjG4NRppU3rzCbx306y-y4QKY",
-    from:"foodi@gmail.com"
-}
+
